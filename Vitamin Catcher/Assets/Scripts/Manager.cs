bbf9f52fbe2c5aed17cd9e80bd87b38d176f1d0e
SayedXAn <anow.sayed@gmail.com>
2025-03-26 18:48:31 +0600

    player settings and build profile
    escape to quit
    r to restart

--- Vitamin Catcher/Assets/Scripts/Manager.cs
+++ Vitamin Catcher/Assets/Scripts/Manager.cs
@@ -1,9 +1,9 @@
-
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
@@ -33,7 +33,14 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
     public void StartSpawn()
     {
